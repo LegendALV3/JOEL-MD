fb2155acbe65208483013c4bfd7819bd7877b659
LegendALV3 <legendalv@duck.com>
2024-08-25 00:20:00 +0530

    Update set.js

--- set.js
+++ set.js
@@ -7,7 +7,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'joel',
+module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "joel_it",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "255714595078",              
